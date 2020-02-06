0b605d2f7249dc8ae288e50acc40b090202fb18b
Mat Belmont <belmontmat@gmail.com>
2020-02-05 16:37:57 -0800

    OAuth implemented

---
+++ public/index.js
@@ -0,0 +1,166 @@
+/*
+ * Mathew Belmont
+ *  11/18/19
+ *  CSE 154 AC/ Tran
+ *
+ *  Homework 4. Practice with Node.js. This file handles the Javascript for the public page.
+ *  This page is populated with data from FETCHES to app.js bookreads API. The page is a list of
+ *  books. If a book is clicked details from the API about it will be pulled up.
+ */
+
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+  var logstate = false;
+
+  /**
+   * Load initial page state and event listeners
+   */
+  function init() {
+    document.getElementById("search").addEventListener("click", beginSearch);
+    document.getElementById("login").addEventListener("click", spotifyLogin);
+  }
+
+  /**
+   * Handles switching from single to all book mode
+   */
+  function beginSearch() {
+    let base = "https://api.spotify.com/v1";
+    fetch()
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(displayResults)
+      .catch(console.error);
+  }
+
+  function displayResults() {
+
+  }
+
+  function spotifyLogin() {
+    window.location.href = "/login";
+    logstate = true; // We should check the reposnse to garuntee this
+  }
+
+  function loggedIn() {
+    document.getElementById("login").classList.add("hidden");
+  }
+
+  /**
+   * Takes a response from a GET fetch to the books endpoint and populates the all books section
+   * @param {Response} jresponse - a JSON API Response object
+   */
+  function processBooks(jresponse) {
+    let container = document.getElementById("all-books");
+    container.innerHTML = "";
+    for (let i = 0; i < jresponse.length; i++) {
+      let bcard = document.createElement("div");
+      let title = document.createElement("p");
+      title.textContent = jresponse[i]["title"];
+      let cover = document.createElement("img");
+      cover.src = "covers/" + jresponse[i]["book_id"] + ".jpg";
+      cover.alt = "cover of " + jresponse[i]["title"];
+      bcard.appendChild(cover);
+      bcard.appendChild(title);
+      bcard.classList.add("selectable");
+      bcard.addEventListener("click", function() {
+        singleBook(jresponse[i]["book_id"]);
+      });
+      container.appendChild(bcard);
+    }
+  }
+
+  /**
+   * Handles populating the single book section after a book is clicked on. It does so by making 3
+   * FETCH calls to the bestreads API
+   * @param {String} id - a unique String used to reference a book in the database
+   */
+  function singleBook(id) {
+    document.getElementById("all-books").classList.add("hidden");
+    document.getElementById("single-book").classList.remove("hidden");
+    let cover = document.getElementById("book-cover");
+    cover.src = "covers/" + id + ".jpg";
+    fetch('/bestreads/description/' + id)
+      .then(checkStatus)
+      .then(resp => resp.text())
+      .then(processDesc)
+      .catch(console.error);
+
+    fetch('/bestreads/info/' + id)
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(processInfo)
+      .catch(console.error);
+
+    fetch('/bestreads/reviews/' + id)
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(processReviews)
+      .catch(console.error);
+  }
+
+  /**
+   * Takes a response from a GET fetch to the description endpoint and populates the description
+   * section
+   * @param {Response} tresponse - a text API Response object
+   */
+  function processDesc(tresponse) {
+    let container = document.getElementById("book-description");
+    container.textContent = tresponse;
+  }
+
+  /**
+   * Takes a response from a GET fetch to the info endpoint and populates the title/author section
+   * @param {Response} jresponse - a JSON API Response object
+   */
+  function processInfo(jresponse) {
+    let author = document.getElementById("book-author");
+    let title = document.getElementById("book-title");
+    author.textContent = jresponse["author"];
+    title.textContent = jresponse["title"];
+  }
+
+  /**
+   * Takes a response from a GET fetch to the reviews endpoint and populates the reviews section
+   * @param {Response} jresponse - a JSON API Response object
+   */
+  function processReviews(jresponse) {
+    let container = document.getElementById("book-reviews");
+    container.innerHTML = "";
+    let avg = 0;
+    for (let i = 0; i < jresponse.length; i++) {
+      let score = jresponse[i]["rating"];
+      avg += score;
+      let reviewer = document.createElement("h3");
+      let rating = document.createElement("h4");
+      let review = document.createElement("p");
+      reviewer.textContent = jresponse[i]["name"];
+      rating.textContent = "Rating: " + score.toFixed(1);
+      review.textContent = jresponse[i]["text"];
+
+      container.appendChild(reviewer);
+      container.appendChild(rating);
+      container.appendChild(review);
+    }
+    avg = avg / jresponse.length;
+    document.getElementById("book-rating").textContent = avg.toFixed(1);
+  }
+
+  /**
+   * display helpful error message on the page
+   * @param {Response} response - an API Response object
+   * @return {Response} the same input
+   */
+  function checkStatus(response) {
+    if (!response.ok) {
+      let output = document.getElementById("output");
+      output.innerHTML = "";
+      let text = document.createElement("p");
+      text.textContent = "Words should begin with a letter";
+      output.appendChild(text);
+      throw Error("Error in request: " + response.statusText);
+    }
+    return response; // a Response object
+  }
+})();
--- server.js
+++ server.js
@@ -8,15 +8,151 @@ const util = require("util");
 const glob = require("glob");
 const fs = require('fs').promises;
 const mysql = require("mysql2/promise");
-const db = mysql.createPool({
+
+// OAuth
+const request = require('request'); // "Request" library
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+
+var client_id = 'b427cfa765fb4f6188db746db180b679'; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = 'http://localhost:8000/callback'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'))
+   .use(cors())
+   .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email playlist-read-collaborative playlist-read-private user-top-read';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+/*const db = mysql.createPool({
   host: process.env.DB_URL || 'localhost',
   port: process.env.DB_PORT || '3306',
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'hw5db'
-});
-const express = require("express");
-const app = express();
+});*/
 const multer = require("multer");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -290,6 +426,5 @@ function getTime() {
     ':' + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
 }
 
-app.use(express.static("public"));
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
